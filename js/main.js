a9f718b1358e41e6b8150484a3522d0246166c3d
Logan Madej <lnmadej23@gmail.com>
2021-04-30 09:55:12 -0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,27 @@
+$(document).ready(() =>{
+    $('.hint-box').on('click', () => {
+      $('.hint').slideToggle(800);
+    })
+  
+    $('.wrong-answer-one').on('click', () => {
+      $('.wrong-text-one').fadeOut('slow');
+      $('.frown').show();
+    })
+    $('.wrong-answer-two').on('click', () => {
+      $('.wrong-text-one').fadeOut('fast');
+      $('.frown').show();
+    })
+    $('wrong-answer-three').on('click', () =>
+    {
+      $('.wrong-text-three').fadeOut();
+      $('.frown').show();
+    });
+  
+    $('.correct-answer').on('click', () => {
+      $('.frown').hide();
+      $('smiley').show();
+      $('.wrong-answer-one').fadeOut('slow');
+      $('.wrong-answer-two').fadeOut('slow');
+      $('.wrong-answer-three').fadeOut('slow');
+    })
+  });
